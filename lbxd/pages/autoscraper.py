f9ffbe3a0a4255917e7b9f7126fcd77a1d683fae
juanjuanjuanfer <2109061@upy.edu.mx>
2024-10-10 20:34:08 -0600

    fixed autoscraper

--- lbxd/pages/autoscraper.py
+++ lbxd/pages/autoscraper.py
@@ -25,6 +25,7 @@ st.markdown("# Autoscraper")
 st.markdown("Login to Interact")
 user = st.text_input("Enter your username:")
 passw = st.text_input("Enter your password:", type="password")
+film_name = st.text_input("Enter movie name:")
 if st.button("Login"):
     # check from secrets.toml
     with open(".streamlit/secrets.toml", "r") as f:
@@ -32,11 +33,10 @@ if st.button("Login"):
         secrets_dict = toml.loads(secrets)
         if user == secrets_dict["autoscraper"]["user"] and passw == secrets_dict["autoscraper"]["pass"]:
 
-            film_name = st.text_input("Enter movie name:")
-            if st.button("Scrape"):
-                for i in range(1000):
-                    autoscraper(film_name)
-                    time.sleep(5)
+
+            for i in range(1000):
+                autoscraper(film_name)
+                time.sleep(5)
         else:
             st.write("Invalid username or password.")
             st.markdown('<img src="https://media1.tenor.com/m/7Rw8rOLsNOEAAAAC/moodeng.gif" width="800"/>', unsafe_allow_html=True)
